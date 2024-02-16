d8067eda6ccbadfe6b1f353176ca9b71f9b60c1a
garrisonma <133896022+garrisonma@users.noreply.github.com>
2024-02-16 19:01:24 +0000

    updates

---
+++ script.js
@@ -0,0 +1,14 @@
+function playSound() {
+    var audio = document.getElementById("sound");
+    audio.play();
+}
+
+function startNoodleAdventure() {
+    alert("Get ready for a noodle-tastic adventure!");
+    // Code for noodle adventure goes here
+}
+
+function activateZap() {
+    alert("Zap-o-Matic activated! Brace yourself for some zany surprises!");
+    // Code for Zap-o-Matic goes here
+}
